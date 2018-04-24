65ef7dec3b9e0453f5d82f90372c61f3e4f938aa
sapphirekuo <sapphirekuo@gmail.com>
2018-04-24 13:36:42 +0800

    generate admin::users controller and add Admin::BaseController then  move authenticate_admin in

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,4 @@
-class Admin::PostsController < ApplicationController
-  before_action :authenticate_admin
+class Admin::PostsController < Admin::BaseController
   
   def index
     @posts = Post.all
