50c96a34b1fd684e8f8b8244f0975cc9c90c5730
sapphirekuo <sapphirekuo@gmail.com>
2018-04-24 12:15:21 +0800

    create user model using devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424032207) do
+ActiveRecord::Schema.define(version: 20180424040801) do
 
   create_table "posts", force: :cascade do |t|
     t.text "description"
@@ -23,6 +23,31 @@ ActiveRecord::Schema.define(version: 20180424032207) do
     t.integer "likes_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "role"
+    t.string "name"
+    t.string "avatar"
+    t.text "introduction"
+    t.integer "replies_count"
+    t.integer "likes_count"
+    t.integer "friends_count"
+    t.integer "inverse_friends_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
