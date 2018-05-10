db21bfb25e103ca9f7a43c58b8d44813297cfb48
sapphirekuo <sapphirekuo@gmail.com>
2018-05-10 16:11:08 +0800

    api controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::PostsController < ApiController
+  def index
+    @posts = Post.all
+    render json: {
+      data: @posts.map do |post|
+        {
+          title: post.title,
+          description: post.description
+         }
+      end
+    }
+  end
+
+  def show
+  @post = Post.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @post.title,
+        description: @post.description
+      }
+    end
 end
