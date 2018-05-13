bdca1b29b03bee30db3de282c6f8ecf3572dbdc6
sapphirekuo <sapphirekuo@gmail.com>
2018-05-14 02:02:09 +0800

    add ransack for category#show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,11 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.readable_by(current_user).page(params[:page]).per(20)
+    # @posts = @category.posts.readable_by(current_user).page(params[:page]).per(20)
+    posts = @category.posts.readable_by(current_user).where(status: "Published")
+    @q = posts.ransack(params[:q])
+
+    @posts = @q.result.order(id: :asc).page(params[:page]).per(20)
+
   end
 end
